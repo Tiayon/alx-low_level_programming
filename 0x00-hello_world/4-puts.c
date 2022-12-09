0e37f0f0cb2069c49660703afe623e1597cf1f3d
Tiayon <djomoubernice@gmail.com>
2022-12-08 18:21:43 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
-/* main - Entry point*/
-/*Return: gives 0 if success*/
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
