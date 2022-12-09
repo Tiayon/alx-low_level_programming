a98f223a3b2bab3d6510cc9775ec5d9a05eb1393
Tiayon <djomoubernice@gmail.com>
2022-12-08 17:02:53 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+  
+{
+  
+  //string initialisation
+  
+  char Mystr[] = "Programming is like building a multilingual puzzle\n";
+  
+
+  
+  puts(Mystr); //writing the string to stdout
+  
+
+  
+  return 0;
+  
+}
