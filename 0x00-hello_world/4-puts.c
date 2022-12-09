6a0b7d42511d0724beeb82b9fb44b1cab1699411
Tiayon <djomoubernice@gmail.com>
2022-12-08 18:14:00 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#include<stdio.h>
 /* main - Entry point*/
 /*Return: gives 0 if success*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);  
+return (0);
 }
