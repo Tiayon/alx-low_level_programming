0a69083ed4239139b287a9037fd62c681c0f02c1
Tiayon <djomoubernice@gmail.com>
2022-12-12 14:58:16 -0800

    printf hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main _entry point
-* return 0 if successful
+*main - entry point
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-	/*prints the text in quotes*/
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+	/*prints the text in quotes*/
 	return (0);
 }
