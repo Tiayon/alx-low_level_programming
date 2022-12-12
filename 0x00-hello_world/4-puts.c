411965587fa819ee55a18f12649cd89de016e69b
Tiayon <djomoubernice@gmail.com>
2022-12-12 05:26:42 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /*main - Entry*/
  /*Description: 'the program's description'*/
  /*Return: Always 0 (Success) */
-int main(void)
+int main (void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
