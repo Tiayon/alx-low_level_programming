e7754ae331f1996e22da51741fba926ccee2a984
Tiayon <djomoubernice@gmail.com>
2022-12-12 04:56:16 -0800

    puts fxn

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
-#include<stdio.h>
-/* main - Entry point*/
-/* Return: Always 0 (Success)*/
-void main(void)
+#include <stdio.h>
+int main(void)
 {
-  /*puts prints string to stdout*/
-char Mystr[] = "\"Programming is like building a multilingual puzzle";
-puts(Mystr);
-return;
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
+
