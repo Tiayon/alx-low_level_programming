2f3d3585556d050c634483cdc6210c9be9d19a93
Tiayon <djomoubernice@gmail.com>
2022-12-14 03:16:56 -0800

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c = 'a'; c--)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	/* inline comment*/
+	return (0);
+}
