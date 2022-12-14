78af7f26c281cce82398b5ed5e21c7a22241cf31
Tiayon <djomoubernice@gmail.com>
2022-12-14 04:33:29 -0800

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	char c;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	for (c= 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	/* inline comment*/
+	return (0);
+}
