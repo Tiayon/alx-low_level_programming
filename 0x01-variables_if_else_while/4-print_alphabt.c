4256b377b799defc77f4faa6cff7fa6e2c67fd3e
Tiayon <djomoubernice@gmail.com>
2022-12-14 02:24:59 -0800

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	if (c != 'q' && c != 'e')
+	{
+		putchar(c);
+	}
+	}
+	/* inline comment*/
+	putchar('\n');
+	return (0);
+}
