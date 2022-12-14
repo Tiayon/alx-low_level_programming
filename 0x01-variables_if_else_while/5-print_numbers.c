95164f744dfe3da262007c6912083a3810f6b823
Tiayon <djomoubernice@gmail.com>
2022-12-14 02:33:51 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d\n", n);
+	}
+	/* inline comment*/
+	return (0);
+}
+
