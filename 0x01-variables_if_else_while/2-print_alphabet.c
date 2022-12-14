6c8665874187dce1952db5212ad3fa6f22c08435
Tiayon <djomoubernice@gmail.com>
2022-12-14 01:12:53 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * REturn: Always 0 (success)
+ */
+int main(void)
+{
+	char l;
+	char a;
+	char z;
+
+	for (l = a; l <= z; l++)
+	{
+		putchar(l);
+	}
+	/* inline comment*/
+
+	return (0);
+}
