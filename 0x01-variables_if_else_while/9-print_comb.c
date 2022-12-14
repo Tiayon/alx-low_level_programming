1b7034b1856033446487cd0d564cd08fb07f4f9c
Tiayon <djomoubernice@gmail.com>
2022-12-14 05:01:07 -0800

    num comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,14 +10,9 @@ int main(void)
 	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
-		for (n = '0'; n <= '8'; n++)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(',');
 	}
 	/* inline comment*/
-	
 	putchar('\n');
 	return (0);
 }
