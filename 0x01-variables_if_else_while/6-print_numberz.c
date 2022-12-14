c6146d3821428ce5e0bc0452ac2ed462612b261e
Tiayon <djomoubernice@gmail.com>
2022-12-14 02:42:44 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	/* code to print single digit numbers*/
+	return (0);
+}
