17c8ba5c988f5ae3fc6c17fcae307089f7abce8d
Tiayon <djomoubernice@gmail.com>
2022-12-14 22:17:47 -0800

    last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,24 +11,10 @@
 int print_last_digit(int n)
 {
 	int r;
-	int l;
-	int a = INT_MIN;
-	r = n % 10;
-	l = a % 10;
 
 	if (n < 0)
-	{
 		n = (-1) * n;
-
+	r = n % 10;
 		_putchar(r + '0');
-	}
-	if (a < 0)
-	{
-		a = -a;
-
-		_putchar(l + '0');
-	}
 	return (r);
-	return (l);
-	
 }
