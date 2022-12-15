53e9a1b10534f4b8b59fc98de33746e76f2bdb16
Tiayon <djomoubernice@gmail.com>
2022-12-14 18:29:53 -0800

    alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,9 +8,10 @@
 void print_alphabet(void)
 {
 	char l;
-	
-	for (l = 'a'; l <= 'z'; l++)
-		_putchar(l);
-	_putchar('\n');
 
+	for (l = 'a'; l<= 'z'; l++)
+	{
+		_putchar(l);
+	}
+	_putchar('\n');
 }
