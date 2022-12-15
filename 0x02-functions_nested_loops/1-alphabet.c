81aaaf3849d9cc370ee123882ad5ed549a09a61e
Tiayon <djomoubernice@gmail.com>
2022-12-14 18:19:24 -0800

    alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * program that prints 
+ * all the alphabet
+ * in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		_putchar(l);
+	_putchar('\n');
+
+}
