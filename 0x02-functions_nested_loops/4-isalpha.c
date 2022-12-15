62bc72a1c1853e943270db1e9a0ce7d655ff59b7
Tiayon <djomoubernice@gmail.com>
2022-12-14 19:38:11 -0800

    is_alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - checks if a letter is uppercase or lowercase
+ *
+ * &c: character to be checked
+ *
+ * Return: returns 1 if the letter is uppercase or lowercase and 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else if (c <= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
