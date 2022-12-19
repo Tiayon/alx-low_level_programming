b7646e85e72de01c244b922877d03d968edd70f1
Tiayon <djomoubernice@gmail.com>
2022-12-19 15:57:19 -0800

    swappy

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps the values of a and b
+ * @a: first parameter of the swap
+ * @b: second parameter of the swap
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 98;
+	*b = 42;
+
+	*a = *b;
+	*b = *a;
+}
