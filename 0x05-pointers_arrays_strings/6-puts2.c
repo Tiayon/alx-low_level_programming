070772f318f6d1437ced3256ad1afc65eab49d2c
Tiayon <djomoubernice@gmail.com>
2022-12-19 18:01:47 -0800

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 -  prints every other character of a string, starting with the first then a newline
+ * puts2 -  prints every other character of a string, the first then a newline
  * @str: pointer to string of which characters are printed
  * return: always 0
  */
