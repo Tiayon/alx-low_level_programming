2b517e280ab030488c4fb0415a0fa223757625ec
Tiayon <djomoubernice@gmail.com>
2022-12-19 18:12:55 -0800

    puts_half

---
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main -checks code
+ * return: always 0
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts_half(str);
+	return 0;
+}
